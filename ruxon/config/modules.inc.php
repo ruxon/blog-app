3241cc52a6d1aab99cc75915f4808ff76d0ec68e
Max Pinyugin <m.pinyugin@gmail.com>
2014-07-14 15:38:03 +0400

    fixed for composer use

--- ruxon/config/modules.inc.php
+++ ruxon/config/modules.inc.php
@@ -1,11 +1,25 @@
-<?php 
+<?php
 
-return array (
-  'Ruxon' => 
-  array (
-    'Name' => 'Главный модуль',
-    'Description' => '',
-    'Version' => '8.0',
-    'DbRevision' => '-1',
-  ),
+return array(
+    'Ruxon' => array(
+        'Name' => 'Главный модуль',
+        'Description' => '',
+        'Version' => '8.0',
+        'DbRevision' => '-1',
+        'BasePath' => 'vendor/ruxon/module-ruxon'
+    ),
+
+    'App' => array(
+        'Name' => 'Модуль приложения',
+        'Description' => '',
+        'Version' => '8.0',
+        'DbRevision' => '-1',
+    ),
+
+    'Blog' => array(
+        'Name' => 'Блог',
+        'Description' => '',
+        'Version' => '8.0',
+        'DbRevision' => '2',
+    ),
 );
