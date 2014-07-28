38ea06655ae83362d5a3a83279208ed41534ff8d
Max Pinyugin <m.pinyugin@gmail.com>
2014-07-28 15:43:31 +0400

    fixed

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
-require_once(dirname(__FILE__).'/vendor/autoload.php');
+
+if (file_exists(dirname(__FILE__).'/vendor/autoload.php')) {
+    require_once(dirname(__FILE__).'/vendor/autoload.php');
+}
+
 require_once(dirname(__FILE__).'/ruxon/kernel/kernel.php');
 
 Core::init(WebApplication::getInstance());
